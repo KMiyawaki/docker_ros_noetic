581ed14c38d70b25c6ff888cf301f505c7e64de7
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2023-07-13 08:38:49 +0900

    add: set sub-groups for user inside docker

--- docker_start.sh
+++ docker_start.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 function main(){
     local -r WS="${HOME}/catkin_ws"
-    local -r SETUP_SCRIPTS="setup_robot_programming"
     if [ ! -e ${WS} ]; then
         mkdir -p ${WS}/src
         echo "Making ${WS}/src"
     fi
-    (
-        cd ${HOME}
-        if [ ! -e ${SETUP_SCRIPTS} ]; then
-            echo "Cloning ${SETUP_SCRIPTS}"
-            git clone https://github.com/KMiyawaki/${SETUP_SCRIPTS}.git
-        else
-            cd ${SETUP_SCRIPTS}
-            git pull
-        fi
-    )
+    local -r GROUP=`id -gn`
+    local SUB_GROUPS=`groups|sed "s/${GROUP}//g"|sed "s/^ *\| *$//g"|sed "s/ /,/g"`
 
-    echo "UID=${UID}" > .env
+    echo "USER=${USER}" > .env
+    echo "UID=${UID}" >> .env
     echo "GID=`id -g`" >> .env
     echo "HOME=${HOME}" >> .env
-    echo "SETUP_SCRIPTS=${SETUP_SCRIPTS}" >> .env
     docker-compose up -d
 }
 
